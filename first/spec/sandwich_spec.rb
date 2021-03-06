5859e06f65e850b75b643fc05d87bfa12f0f016b
Daniel Penaloza <danielpenalozatj@gmail.com>
2021-03-05 19:01:40 -0800

    Refactoring a Struct

--- first/spec/sandwich_spec.rb
+++ first/spec/sandwich_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe 'An ideal sandwich' do
   it 'is deilicious' do
-    sandwich = Sandwich.new('delicious', [])
+    # In this way we are supporting keyword arguments.
+    sandwich = Sandwich.new(taste: 'delicious', toppings: ['beef', 'lettuce', 'tommatoes', 'spinach'])
     
     taste = sandwich.taste
     expect(taste).to eq('delicious')
   end
 end
-Sandwich = Struct.new(:taste, :toppings)
+
+# Sandwich = Struct.new(:taste, :toppings)
+# Refactoring the Struct to a best approach and avoiding mistakes in the futures
+Sandwich = Struct.new(:taste, :toppings, keyword_init: true)
