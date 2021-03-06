3fcfd757b22b9fa59ce14fb4b6e600a3bb2107e1
Daniel Penaloza <danielpenalozatj@gmail.com>
2021-03-05 19:30:44 -0800

    Adding a before hook to the creation of the value object

--- first/spec/sandwich_spec.rb
+++ first/spec/sandwich_spec.rb
@@ -1,13 +1,19 @@
 RSpec.describe 'An ideal sandwich' do
+  # Creating a before hook to initialize an object
+  before { @sandwich = Sandwich.new(taste: 'delicious', toppings: [])}
   it 'is deilicious' do
-    # In this way we are supporting keyword arguments.
-    sandwich = Sandwich.new(taste: 'delicious', toppings: ['beef', 'lettuce', 'tommatoes', 'spinach'])
-    
-    taste = sandwich.taste
+    taste = @sandwich.taste
     expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    toppings = @sandwich.toppings << 'cheese'
+
+    expect(toppings).not_to be_empty
   end
 end
 
 # Sandwich = Struct.new(:taste, :toppings)
 # Refactoring the Struct to a best approach and avoiding mistakes in the futures
+# Adding the way to supporting keyword arguments when we create an instance of the Sandwich Struct Class.
 Sandwich = Struct.new(:taste, :toppings, keyword_init: true)
